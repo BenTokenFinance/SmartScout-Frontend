fc0c3d55da93e10bb5e708d5a6fee0812ae2e1b7
ingmgni <ingmgni@github.com>
2024-02-27 13:11:56 +0800

    Update env

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ git pull origin sbch
 docker build --build-arg GIT_COMMIT_SHA=$(git rev-parse --short HEAD) --build-arg GIT_TAG=$(git describe --tags --abbrev=0) -t blockscout-frontend:local ./
 
 # ????docker-composeĿ¼
-docker run -p 3000:3000 --env-file .env blockscout-frontend:local -d
+docker run -p 3000:3000 --env-file .env -d  blockscout-frontend:local
 
 echo "frentend start"

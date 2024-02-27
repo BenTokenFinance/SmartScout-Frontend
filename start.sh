870a19be856363c57967045f8938c7da20169364
ingmgni <ingmgni@github.com>
2024-02-27 11:57:49 +0800

    Update env

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ????docker-composeĿ¼
-npm run build:docker
+docker build --build-arg GIT_COMMIT_SHA=$(git rev-parse --short HEAD) --build-arg GIT_TAG=$(git describe --tags --abbrev=0) -t blockscout-frontend:local ./
 
 # ????docker-composeĿ¼
 docker run -p 3000:3000 --env-file .env blockscout-frontend:local -d

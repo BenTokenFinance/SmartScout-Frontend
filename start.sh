3b3a23d16b177a0481a5bbf1afac6283b3765e35
ingmgni <ingmgni@github.com>
2024-03-14 14:26:37 +0800

    UPdate env

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ git pull origin sbch
 # ????docker-composeĿ¼
 docker build --build-arg GIT_COMMIT_SHA=$(git rev-parse --short HEAD) --build-arg GIT_TAG=$(git describe --tags --abbrev=0) -t blockscout-frontend:local ./
 
+docker stop $(docker ps -q --filter ancestor=blockscout-frontend:local)
+
 # ????docker-composeĿ¼
 docker run -p 3000:3000 --env-file .env -d  blockscout-frontend:local
 
